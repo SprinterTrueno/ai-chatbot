5a415b94e733ae77e3f407cf301249b18ede9d8b
TaKumi <war3_th000@qq.com>
2024-12-03 22:09:07 +0800

    feat: 页面

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
-import { Result } from "antd";
-import ReactCartoonLogo from "@/assets/images/react-cartoon-logo.png";
+import { Input, Button } from "antd";
+import { SendOutlined } from "@ant-design/icons";
+import "./index.less";
 
 const Homepage: FC = () => {
   return (
-    <Result
-      style={{ paddingTop: "15vh" }}
-      icon={
-        <img src={ReactCartoonLogo} width={391} alt="react-cartoon-logo.png" />
-      }
-      title="The library for web and native user interfaces"
-    />
+    <div className="container">
+      <div className="header">
+        <Button type="primary">新建对话</Button>
+      </div>
+      <div className="content">
+        <div>这是内容区</div>
+      </div>
+      <div className="footer">
+        <Input placeholder="请输入" size="large" />
+        <Button className="submit" icon={<SendOutlined />} size="large" />
+      </div>
+    </div>
   );
 };
 
