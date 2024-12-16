10da568a65cb9e2d7366b7d14c1a2cc80fbfbefc
TaKumi <war3_th000@qq.com>
2024-12-16 18:13:45 +0800

    feat(ai-chatbot): 实现 AI 聊天机器人的核心功能
    - 添加用户输入和聊天历史的管理逻辑
    - 实现消息发送和接收功能
    - 优化页面样式，增加用户和助手的消息展示
    - 更新项目依赖，包括 nanoid、openai 等

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,20 +1,84 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Input, Button } from "antd";
 import { SendOutlined } from "@ant-design/icons";
+import { nanoid } from "nanoid";
 import "./index.less";
 
+const INITIAL_CHAT_HISTORY = [
+  { role: "system", content: "You are a helpful assistant." },
+];
+
 const Homepage: FC = () => {
+  const [chatHistory, setChatHistory] = useState(INITIAL_CHAT_HISTORY);
+
+  const [inputValue, setInputValue] = useState();
+
+  const handleCreateNewChat = () => {
+    setInputValue(null);
+    setChatHistory(INITIAL_CHAT_HISTORY);
+  };
+
+  /**
+   * 发送消息
+   */
+  const handleSendMessage = async () => {
+    if (!inputValue) {
+      return;
+    }
+
+    const newChatHistory = [
+      ...chatHistory,
+      { role: "user", content: inputValue },
+    ];
+
+    setInputValue(null);
+    setChatHistory(newChatHistory);
+
+    fetch("http://101.201.154.135/ai-chatbot-api/qwen/conversation", {
+      method: "POST",
+      body: JSON.stringify(newChatHistory),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => {
+      res.json().then((data) => {
+        setChatHistory([...newChatHistory, data]);
+      });
+    });
+  };
+
   return (
     <div className="container">
       <div className="header">
-        <Button type="primary">新建对话</Button>
+        <Button type="primary" onClick={handleCreateNewChat}>
+          新建对话
+        </Button>
       </div>
       <div className="content">
-        <div>这是内容区</div>
+        {chatHistory.map((item, index) => {
+          return (
+            index !== 0 && (
+              <div key={nanoid()} className={item.role}>
+                <p>{item.content}</p>
+              </div>
+            )
+          );
+        })}
       </div>
       <div className="footer">
-        <Input placeholder="请输入" size="large" />
-        <Button className="submit" icon={<SendOutlined />} size="large" />
+        <Input
+          placeholder="请输入"
+          size="large"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          onPressEnter={handleSendMessage}
+        />
+        <Button
+          className="submit"
+          icon={<SendOutlined />}
+          size="large"
+          onClick={handleSendMessage}
+        />
       </div>
     </div>
   );
