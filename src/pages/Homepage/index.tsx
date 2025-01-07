6dfd6b2c4d0fcb80dd57563c50ca266f886adc9e
TaKumi <war3_th000@qq.com>
2025-01-07 23:59:04 +0800

    feat(Homepage): 支持图片上传并优化聊天记录展示
    - 新增图片上传功能，用户可以上传图片并发送
    - 重构聊天记录展示逻辑，支持文本和图片的组合消息
    - 优化聊天项的样式和布局，提升用户体验
    - 添加上传中的加载状态和错误提示

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,17 +1,35 @@
 import { FC, useState } from "react";
-import { Input, Button } from "antd";
-import { SendOutlined } from "@ant-design/icons";
+import { Input, Button, Upload, message } from "antd";
+import { LoadingOutlined, PlusOutlined, SendOutlined } from "@ant-design/icons";
 import { nanoid } from "nanoid";
 import "./index.less";
+
+type TextContent = string;
+type ImageAndTextContent = {
+  type: string;
+  text?: string;
+  image_url?: { url: string };
+}[];
+
+interface ChatHistory {
+  role: string;
+  content: TextContent | ImageAndTextContent;
+}
 
 const INITIAL_CHAT_HISTORY = [
   { role: "system", content: "You are a helpful assistant." },
 ];
 
+const BASE_URL = "http://localhost:3001";
+// const BASE_URL = "http://101.201.154.135";
+
 const Homepage: FC = () => {
-  const [chatHistory, setChatHistory] = useState(INITIAL_CHAT_HISTORY);
+  const [chatHistory, setChatHistory] =
+    useState<ChatHistory[]>(INITIAL_CHAT_HISTORY);
 
   const [inputValue, setInputValue] = useState();
+  const [imageUrl, setImageUrl] = useState<string>();
+  const [uploading, setUploading] = useState(false);
 
   const handleCreateNewChat = () => {
     setInputValue(null);
@@ -28,21 +46,31 @@ const Homepage: FC = () => {
 
     const newChatHistory = [
       ...chatHistory,
-      { role: "user", content: inputValue },
+      {
+        role: "user",
+        content: imageUrl
+          ? [
+              { type: "text", text: inputValue },
+              { type: "image_url", image_url: { url: imageUrl } },
+            ]
+          : inputValue,
+      },
     ];
 
     setInputValue(null);
+    setImageUrl(null);
     setChatHistory(newChatHistory);
 
-    fetch("http://101.201.154.135/ai-chatbot-api/qwen/conversation", {
+    const res = await fetch(`${BASE_URL}/ai-chatbot-api/qwen/conversation`, {
       method: "POST",
       body: JSON.stringify(newChatHistory),
       headers: { "Content-Type": "application/json" },
-    }).then((res) => {
-      res.json().then((data) => {
-        setChatHistory([...newChatHistory, data]);
-      });
     });
+
+    const data = await res.json();
+
+    setChatHistory([...newChatHistory, data]);
+    console.log([...newChatHistory, data]);
   };
 
   return (
@@ -54,16 +82,101 @@ const Homepage: FC = () => {
       </div>
       <div className="content">
         {chatHistory.map((item, index) => {
-          return (
-            index !== 0 && (
-              <div key={nanoid()} className={item.role}>
-                <p>{item.content}</p>
-              </div>
-            )
-          );
+          console.log(item);
+
+          if (typeof item.content === "string") {
+            return (
+              index !== 0 && (
+                <div
+                  key={nanoid()}
+                  className={`conversation-item ${item.role}`}
+                >
+                  <div className="item-container">
+                    <div className="item-content">{item.content}</div>
+                  </div>
+                </div>
+              )
+            );
+          }
+
+          if (Array.isArray(item.content)) {
+            return (
+              index !== 0 && (
+                <div
+                  key={nanoid()}
+                  className={`conversation-item ${item.role}`}
+                >
+                  {item.content.map((content) => {
+                    if (content.type === "text") {
+                      return (
+                        <div className="item-container">
+                          <div className="item-content">{content.text}</div>
+                        </div>
+                      );
+                    }
+
+                    if (content.type === "image_url") {
+                      return (
+                        // TODO: 图片预览功能
+                        <div className="todo">
+                          <img
+                            src={content.image_url.url}
+                            alt="avatar"
+                            style={{ width: "100%" }}
+                          />
+                        </div>
+                      );
+                    }
+
+                    return [];
+                  })}
+                </div>
+              )
+            );
+          }
+
+          return [];
         })}
       </div>
       <div className="footer">
+        <Upload
+          name="file123"
+          method="POST"
+          listType="picture-card"
+          showUploadList={false}
+          action={`${BASE_URL}/ai-chatbot-api/qwen/upload-file`}
+          // TODO: 优化上传文件
+          // beforeUpload={beforeUpload}
+          onChange={(info) => {
+            console.log(info);
+            const { status, response } = info.file;
+
+            if (status === "uploading") {
+              setUploading(true);
+              return;
+            }
+
+            if (status === "error") {
+              setUploading(false);
+              message.error(response.message);
+              return;
+            }
+
+            if (status === "done") {
+              setUploading(false);
+              setImageUrl(response.url);
+            }
+          }}
+        >
+          {imageUrl ? (
+            <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+          ) : (
+            <button type="button" style={{ border: 0, background: "none" }}>
+              {uploading ? <LoadingOutlined /> : <PlusOutlined />}
+              <div style={{ marginTop: 8 }}>Upload</div>
+            </button>
+          )}
+        </Upload>
         <Input
           placeholder="请输入"
           size="large"
