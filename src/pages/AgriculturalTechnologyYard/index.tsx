6a905192cf979d7c2f4e6cfb71c0908e1cc04be8
oe_malin <malin_hz@hnzycfc.com>
2025-01-22 16:57:42 +0800

    feat(AgriculturalTechnologyYard): 流式传输-未完成

--- src/pages/AgriculturalTechnologyYard/index.tsx
+++ src/pages/AgriculturalTechnologyYard/index.tsx
@@ -17,6 +17,8 @@ const AgriculturalTechnologyYard: FC = () => {
   const [inputValue, setInputValue] = useState<string>();
   const [sessionId, setSessionId] = useState<string>();
 
+  const [data, setData] = useState([]);
+
   const handleCreateNewChat = () => {
     setInputValue(null);
     setChatHistory([]);
@@ -26,7 +28,37 @@ const AgriculturalTechnologyYard: FC = () => {
    * 发送消息
    */
   const handleSendMessage = async () => {
-    if (!inputValue) {
+    const response = await fetch(API_URLS.atYardCallDashScope, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "text/event-stream",
+      },
+      body: JSON.stringify({ sessionId, text: inputValue }),
+    });
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    let done = false;
+
+    while (!done) {
+      // eslint-disable-next-line no-await-in-loop
+      const { value, done: streamDone } = await reader.read();
+      done = streamDone;
+      if (value) {
+        const chunk = decoder.decode(value, { stream: true });
+        // Process each line as a separate event
+        const lines = chunk
+          .split("\n")
+          .filter((line) => line.startsWith("data: "));
+        lines.forEach((line, index) => {
+          const parsedData = JSON.parse(line.slice(6)); // Remove "data: " prefix
+          setData((prevData) => [...prevData, parsedData.chunk]);
+        });
+      }
+    }
+
+    /* if (!inputValue) {
       return;
     }
 
@@ -53,13 +85,18 @@ const AgriculturalTechnologyYard: FC = () => {
     console.log(data);
 
     setChatHistory([...newChatHistory, { role: "assistant", ...data }]);
-    console.log([...newChatHistory, { role: "assistant", ...data }]);
+    console.log([...newChatHistory, { role: "assistant", ...data }]); */
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <Button type="primary" onClick={handleCreateNewChat}>
+        <Button
+          type="primary"
+          onClick={() => {
+            console.log(data);
+          }}
+        >
           新建对话
         </Button>
       </div>
